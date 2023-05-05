4794aef145d1eb7e0eaa68020fff9ccf03481501
hp1heloisa <heloisapimentel2202@gmail.com>
2023-05-05 20:41:16 -0300

    versao com datas testes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,6 @@
 export default function Post(props) {
     return(
-        <div class="post">
+        <div class="post" data-test="post">
           <div class="topo">
             <div class="usuario">
               <img src={props.srcUsuario} alt={props.altUsuario}/>
@@ -11,22 +11,22 @@ export default function Post(props) {
             </div>
           </div>
           <div class="conteudo">
-            <img src={props.srcConteudo} alt={props.altConteudo}/>
+            <img src={props.srcConteudo} alt={props.altConteudo} data-test="post-image"/>
           </div>
           <div class="fundo">
             <div class="acoes">
               <div>
-                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="heart-outline" data-test="like-post"></ion-icon>
                 <ion-icon name="chatbubble-outline"></ion-icon>
                 <ion-icon name="paper-plane-outline"></ion-icon>
               </div>
               <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
+                <ion-icon name="bookmark-outline" data-test="save-post"></ion-icon>
               </div>
             </div>
             <div class="curtidas">
               <img src={props.srcCurtidas} alt={props.altCurtidas}/>
-              <div class="texto">
+              <div class="texto" data-test="likes-number">
                 {props.texto}
               </div>
             </div>
